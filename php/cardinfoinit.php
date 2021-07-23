35ae980aa1b9956c49e3cb9def138e216c3159b6
David <dsk99711@uga.edu>
2021-07-23 16:06:17 -0400

    updated card keys into less lines

--- php/cardinfoinit.php
+++ php/cardinfoinit.php
@@ -5,25 +5,10 @@ $createcards = "CREATE TABLE IF NOT EXISTS `paymentinfo` (
     `user` int(6) UNSIGNED,
     `type` varchar(255),
     `number` varchar(255),
-    `exp` varchar(255)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+    `exp` varchar(255),
+    PRIMARY KEY (`cardID`),
+    FOREIGN KEY (`user`) REFERENCES `userinfo`(`userID`)    
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
 
 $db->exec($createcards);
-
-$initcards = "ALTER TABLE `paymentinfo`
-    ADD PRIMARY KEY (`cardID`),
-    ADD KEY `userinfo.userID` (`user`)";
-
-$db->exec($initcards);  
-
-
-$more = "ALTER TABLE `paymentinfo`
-    MODIFY `cardID` int(11) NOT NULL AUTO_INCREMENT";
-
-$db->exec($more);
-
-$evenmore = "ALTER TABLE `paymentinfo`
-ADD CONSTRAINT `paymentinfo_ibfk_1` FOREIGN KEY (`user`) REFERENCES `userinfo` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE";
-
-$db->exec($evenmore);
 ?>

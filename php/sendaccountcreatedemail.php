5233e615a8ef1a6aa627e77dbe3890af6bb4a60c
= <=>
2021-08-01 23:51:20 -0400

    email bug

--- php/sendaccountcreatedemail.php
+++ php/sendaccountcreatedemail.php
@@ -13,8 +13,7 @@ use Postmark\PostmarkClient;
 
 $client = new PostmarkClient("5b163fe7-e191-433d-bedb-a85c198f2b24");
 $fromEmail = "griffinhines@uga.edu";
-#$toEmail = filter_input(INPUT_POST, 'email');
-$toEmail = "griffinhines@uga.edu";
+$toEmail = filter_input(INPUT_POST, 'email');
 $subject = "Account created";
 $htmlBody = "An account has been created under this email address";
 $textBody = "";
